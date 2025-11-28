7923383ad5eda405b82daf3b65e169c540ad18e7
M. Hikari Reiziq Rakhadinta <reiziqzip@gmail.com>
2025-11-28 13:29:09 +0700

    namabahin nameserver

---
+++ script_v2/revendell.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+apt update
+apt install isc-dhcp-relay -y
+
+cat << EOF > /etc/default/isc-dhcp-relay
+SERVERS="10.91.1.194"
+INTERFACESv4="eth1 eth0"
+EOF
+
+service isc-dhcp-relay restart
+
+echo "nameserver 10.91.1.195" > /etc/resolv.conf
